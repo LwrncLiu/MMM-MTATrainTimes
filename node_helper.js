22f320f2133a8538e18322eec62a7a542455ea24
LwrncLiu <lwrncliu@gmail.com>
2025-01-19 16:44:08 -0600

    use one object instead of two maps

--- node_helper.js
+++ node_helper.js
@@ -6,96 +6,58 @@ module.exports = NodeHelper.create({
     start: function () {
         this.apiBase = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm";
         console.log("Starting node helper for" + this.name);
-        this.stopIdMap = new Map([
-            ["D43", ["D43N", "D43S"]],
-            ["B23", ["B23N", "B23S"]],
-            ["B22", ["B22N", "B22S"]],
-            ["B21", ["B21N", "B21S"]],
-            ["B20", ["B20N", "B20S"]],
-            ["B19", ["B19N", "B19S"]],
-            ["B18", ["B18N", "B18S"]],
-            ["B17", ["B17N", "B17S"]],
-            ["B16", ["B16N", "B16S"]],
-            ["B15", ["B15N", "B15S"]],
-            ["B14", ["B14N", "B14S"]],
-            ["B13", ["B13N", "B13S"]],
-            ["B12", ["B12N", "B12S"]],
-            ["R36", ["R36N", "R36S"]],
-            ["R31", ["R31N", "R31S"]],
-            ["D22", ["D22N", "D22S"]],
-            ["D21", ["D21N", "D21S"]],
-            ["D20", ["D20N", "D20S"]],
-            ["D17", ["D17N", "D17S"]],
-            ["D16", ["D16N", "D16S"]],
-            ["D15", ["D15N", "D15S"]],
-            ["D14", ["D14N", "D14S"]],
-            ["A24", ["A24N", "A24S"]],
-            ["A22", ["A22N", "A22S"]],
-            ["A21", ["A21N", "A21S"]],
-            ["A20", ["A20N", "A20S"]],
-            ["A19", ["A19N", "A19S"]],
-            ["A18", ["A18N", "A18S"]],
-            ["A17", ["A17N", "A17S"]],
-            ["A16", ["A16N", "A16S"]],
-            ["A15", ["A15N", "A15S"]],
-            ["D13", ["D13N", "D13S"]],
-            ["D12", ["D12N", "D12S"]],
-            ["D11", ["D11N", "D11S"]],
-            ["D10", ["D10N", "D10S"]],
-            ["D09", ["D09N", "D09S"]],
-            ["D08", ["D08N", "D08S"]],
-            ["D07", ["D07N", "D07S"]],
-            ["D06", ["D06N", "D06S"]],
-            ["D05", ["D05N", "D05S"]],
-            ["D04", ["D04N", "D04S"]],
-            ["D03", ["D03N", "D03S"]],
-            ["D01", ["D01N", "D01S"]],
-        ])
-        this.stopNameMap = new Map([
-            ["D43", "Coney Island-Stillwell Av"],
-            ["B23", "Bay 50 St"],
-            ["B22", "25 Av"],
-            ["B21", "Bay Pkwy"],
-            ["B20", "20 Av"],
-            ["B19", "18 Av"],
-            ["B18", "79 St"],
-            ["B17", "71 St"], 
-            ["B16", "62 St"],
-            ["B15", "55 St"],
-            ["B14", "50 St"],
-            ["B13", "Fort Hamilton Pkwy"],
-            ["B12", "9 Av"],
-            ["R36", "36 St"],
-            ["R31", "Atlantic Av-Barclays Ctr"], 
-            ["D22", "Grand St"], 
-            ["D21", "Broadway-Lafayette St"], 
-            ["D20", "W 4 St-Wash Sq"], 
-            ["D17", "34 St-Herald Sq"], 
-            ["D16", "42 St-Bryant Pk"], 
-            ["D15", "47-50 Sts-Rockefeller Ctr"], 
-            ["D14", "7 Av"], 
-            ["A24", "59 St-Columbus Circle"], 
-            ["A22", "72 St"], 
-            ["A21", "81 St-Museum of Natural History"], 
-            ["A20", "86 ST"], 
-            ["A19", "96 St"], 
-            ["A18", "103 St"], 
-            ["A17", "Cathedral Pkwy (110 St)"], 
-            ["A16", "116 St"], 
-            ["A15", "125 ST"], 
-            ["D13", "145 St"], 
-            ["D12", "155 St"], 
-            ["D11", "161 St-Yankee Stadium"], 
-            ["D10", "167 St"], 
-            ["D09", "180 St"], 
-            ["D08", "174-175 Sts"], 
-            ["D07", "Tremont Av"], 
-            ["D06", "182-183 StS"], 
-            ["D05", "Fordham Rd"], 
-            ["D04", "Kingsbridge Rd"], 
-            ["D03", "Bedford Park Blvd"], 
-            ["D01", "Norwood-205 St"]
-        ])
+        this.parentStops = {
+            "A15": {"childStops": ["A15N", "A15S"], "stopName": "125 ST"},
+            "A16": {"childStops": ["A16N", "A16S"], "stopName": "116 St"},
+            "A17": {"childStops": ["A17N", "A17S"], "stopName": "Cathedral Pkwy (110 St)"},
+            "A18": {"childStops": ["A18N", "A18S"], "stopName": "103 St"},
+            "A19": {"childStops": ["A19N", "A19S"], "stopName": "96 St"},
+            "A20": {"childStops": ["A20N", "A20S"], "stopName": "86 ST"},
+            "A21": {"childStops": ["A21N", "A21S"], "stopName": "81 St-Museum of Natural History"},
+            "A22": {"childStops": ["A22N", "A22S"], "stopName": "72 St"},
+            "A24": {"childStops": ["A24N", "A24S"], "stopName": "59 St-Columbus Circle"},
+            "B12": {"childStops": ["B12N", "B12S"], "stopName": "9 Av"},
+            "B13": {"childStops": ["B13N", "B13S"], "stopName": "Fort Hamilton Pkwy"},
+            "B14": {"childStops": ["B14N", "B14S"], "stopName": "50 St"},
+            "B15": {"childStops": ["B15N", "B15S"], "stopName": "55 St"},
+            "B16": {"childStops": ["B16N", "B16S"], "stopName": "62 St"},
+            "B17": {"childStops": ["B17N", "B17S"], "stopName": "71 St"},
+            "B18": {"childStops": ["B18N", "B18S"], "stopName": "79 St"},
+            "B19": {"childStops": ["B19N", "B19S"], "stopName": "18 Av"},
+            "B20": {"childStops": ["B20N", "B20S"], "stopName": "20 Av"},
+            "B21": {"childStops": ["B21N", "B21S"], "stopName": "Bay Pkwy"},
+            "B22": {"childStops": ["B22N", "B22S"], "stopName": "25 Av"},
+            "B23": {"childStops": ["B23N", "B23S"], "stopName": "Bay 50 St"},
+            "D01": {"childStops": ["D01N", "D01S"], "stopName": "Norwood-205 St"},
+            "D03": {"childStops": ["D03N", "D03S"], "stopName": "Bedford Park Blvd"},
+            "D04": {"childStops": ["D04N", "D04S"], "stopName": "Kingsbridge Rd"},
+            "D05": {"childStops": ["D05N", "D05S"], "stopName": "Fordham Rd"},
+            "D06": {"childStops": ["D06N", "D06S"], "stopName": "182-183 StS"},
+            "D07": {"childStops": ["D07N", "D07S"], "stopName": "Tremont Av"},
+            "D08": {"childStops": ["D08N", "D08S"], "stopName": "174-175 Sts"},
+            "D09": {"childStops": ["D09N", "D09S"], "stopName": "180 St"},
+            "D10": {"childStops": ["D10N", "D10S"], "stopName": "167 St"},
+            "D11": {"childStops": ["D11N", "D11S"], "stopName": "161 St-Yankee Stadium"},
+            "D12": {"childStops": ["D12N", "D12S"], "stopName": "155 St"},
+            "D13": {"childStops": ["D13N", "D13S"], "stopName": "145 St"},
+            "D14": {"childStops": ["D14N", "D14S"], "stopName": "7 Av"},
+            "D15": {"childStops": ["D15N", "D15S"], "stopName": "47-50 Sts-Rockefeller Ctr"},
+            "D16": {"childStops": ["D16N", "D16S"], "stopName": "42 St-Bryant Pk"},
+            "D17": {"childStops": ["D17N", "D17S"], "stopName": "34 St-Herald Sq"},
+            "D20": {"childStops": ["D20N", "D20S"], "stopName": "W 4 St-Wash Sq"},
+            "D21": {"childStops": ["D21N", "D21S"], "stopName": "Broadway-Lafayette St"},
+            "D22": {"childStops": ["D22N", "D22S"], "stopName": "Grand St"},
+            "D25": {"childStops": ["D25N", "D25S"], "stopName": "7 Av"},
+            "D26": {"childStops": ["D26N", "D26S"], "stopName": "Prospect Park"},
+            "D28": {"childStops": ["D28N", "D28S"], "stopName": "Church Av"},
+            "D31": {"childStops": ["D31N", "D31S"], "stopName": "Newkirk Plaza"},
+            "D35": {"childStops": ["D35N", "D35S"], "stopName": "Kings Hwy"},
+            "D39": {"childStops": ["D39N", "D39S"], "stopName": "Sheepshead Bay"},
+            "D40": {"childStops": ["D40N", "D40S"], "stopName": "Brighton Beach"},
+            "D43": {"childStops": ["D43N", "D43S"], "stopName": "Coney Island-Stillwell Av"},
+            "R31": {"childStops": ["R31N", "R31S"], "stopName": "Atlantic Av-Barclays Ctr"},
+            "R36": {"childStops": ["R36N", "R36S"], "stopName": "36 St"},
+        };
     },
 
     socketNotificationReceived: function (notification, payload) {
@@ -112,7 +74,7 @@ module.exports = NodeHelper.create({
                 });
         } else if (notification === "GET_STOP_NAME") {
             const stopId = payload.stopId;
-            const stopName = this.stopNameMap.get(stopId);
+            const stopName = this.parentStops[stopId]["stopName"];
             this.sendSocketNotification("STOP_NAME", {stopId: stopId, stopName: stopName});
         }
     },
@@ -137,13 +99,13 @@ module.exports = NodeHelper.create({
                     let lastStop = null;
 
                     for (let update of stopTimeUpdates) {
-                        if (this.stopIdMap.get(stopId).includes(update.stopId)) {
+                        if (this.parentStops[stopId]["childStops"].includes(update.stopId)) {
                             stopArrivalTime = update.arrival.time;
                         };
                     };
 
                     if (stopArrivalTime) {
-                        lastStop = this.stopNameMap.get(stopTimeUpdates[stopTimeUpdates.length - 1].stopId.slice(0, -1));
+                        lastStop = this.parentStops[stopTimeUpdates[stopTimeUpdates.length - 1].stopId.slice(0, -1)]["stopName"];
                     };
                     if (lastStop) {
                         futureArrivals.push({
